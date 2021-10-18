e2c33a090bb8f7b26342a7f7e6fe091cfb136374
Pedro Henrique de Sousa <pedrozica.phs21@gmail.com>
2021-10-18 09:59:30 -0300

    adicionando menu a activity

--- app/src/main/java/com/example/calculadoraimc/SignUpActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/SignUpActivity.kt
@@ -2,10 +2,57 @@ package com.example.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var editEmail: EditText
+    lateinit var editSenha: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        editEmail = findViewById<EditText>(R.id.edit_text_email)
+
+        supportActionBar!!.title = "Novo Usuário"
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.menu_novo_usuario, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (validarCampos()){
+            //Salvar o registro
+        }
+
+        return true
+
+    }
+
+    fun validarCampos(): Boolean {
+    var valido = true
+
+        if (editEmail.text.isEmpty()) {
+            editEmail.error = "O e-mail é obrigatório!"
+            valido = false
+        }
+
+        if (editSenha.text.isEmpty()) {
+            editSenha.error = "O e-mail é obrigatório!"
+            valido = false
+        }
+
+        return true
+
     }
 }
